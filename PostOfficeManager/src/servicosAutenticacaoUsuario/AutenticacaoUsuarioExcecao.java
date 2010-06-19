3704fa4403be05a8fe57bc1115f533a760e3fdf6
werton007 <werton007@7978b9b3-e860-e8e4-5974-79d8b3795186>
2010-06-19 01:21:17 +0000

    Soh tirando a acentuacao pra rodar o XML.

--- PostOfficeManager/src/servicosAutenticacaoUsuario/AutenticacaoUsuarioExcecao.java
+++ PostOfficeManager/src/servicosAutenticacaoUsuario/AutenticacaoUsuarioExcecao.java
@@ -10,57 +10,57 @@ package servicosAutenticacaoUsuario;
  */
 public class AutenticacaoUsuarioExcecao extends Exception{
 
-    // Código da Exceção.
+    // Codigo da Excecao.
     private Integer codigoExcecao = null;
 
     /**
-     * Construtor padrão(default).
+     * Construtor padrao(default).
      */
     public AutenticacaoUsuarioExcecao(){
-    }// fim do construtor padrão.
+    }// fim do construtor padrao.
 
     /**
-     * Constroi uma exceção de autenticação de usuário a partir
+     * Constroi uma excecao de autenticacao de usuario a partir
      * de uma mensagem.
      * @param mensagem
-     *      A mensagem da exceção.
+     *      A mensagem da excecao.
      */
     public AutenticacaoUsuarioExcecao(String mensagem){
         super(mensagem);
     }// fim do construtor a partir de String mensagem.
 
     /**
-     * Constroi uma exceção de autenticação de usuário a partir
-     * de um código da exceção.
-     * Tabela de códigos da exceção:
+     * Constroi uma excecao de autenticacao de usuario a partir
+     * de um codigo da excecao.
+     * Tabela de codigos da excecao:
      *      1 -  Login : nulo
      *      2 -  Login : vazio
-     *      3 -  Login : caractere(s) inválido(s) - Não alfanumérico
-     *      4 -  Login : tamanho inferior ao mínimo(8)
-     *      5 -  Login : tamanho superior ao máximo(12)
+     *      3 -  Login : caractere(s) invalido(s) - Nao alfanumerico
+     *      4 -  Login : tamanho inferior ao minimo(8)
+     *      5 -  Login : tamanho superior ao maximo(12)
      *      6 -  Senha : nulo
      *      7 -  Senha : vazio
-     *      8 -  Senha : tamanho inferior ao mínimo(8)
-     *      9 -  Senha : tamanho superior ao máximo(12)
+     *      8 -  Senha : tamanho inferior ao minimo(8)
+     *      9 -  Senha : tamanho superior ao maximo(12)
      *      10 - Login : Login incorreto
      *      11 - Senha : Senha incorreta
-     *      12 - Cadastro : Usuario já cadastrado
+     *      12 - Cadastro : Usuario ja cadastrado
      *      
      *
      * @param codigoExcecao
-     *      O código da exceção.
+     *      O codigo da excecao.
      */
     public AutenticacaoUsuarioExcecao(Integer codigoExcecao){
         this.codigoExcecao = codigoExcecao;
     }// fim do construtor a partir de Integer codicoExcecao.
 
     /**
-     * Constroi uma exceção de autenticação de usuário a partir de uma mensagem e
-     * um código de exceção.
+     * Constroi uma excecao de autenticacao de usuario a partir de uma mensagem e
+     * um codigo de excecao.
      * @param mensagem
-     *      A mensagem de exceção.
+     *      A mensagem de excecao.
      * @param codigoExcecao
-     *      O código de exceção.
+     *      O codigo de excecao.
      */
     public AutenticacaoUsuarioExcecao(String mensagem, Integer codigoExcecao){
         super(mensagem);
@@ -68,20 +68,20 @@ public class AutenticacaoUsuarioExcecao extends Exception{
     }// fim do construtor a partir de String mensagem e Integer codigoExcecao.
 
     /**
-     * Recupera o código de exceção.
+     * Recupera o codigo de excecao.
      * @return
-     *      O código de exceção.
+     *      O codigo de excecao.
      */
     public Integer getCodigoExcecao(){
         return this.codigoExcecao;
-    }// fim do método getCodigoExcecao
+    }// fim do metodo getCodigoExcecao
 
     /**
-     * Obtém a mensagem da exceção de autenticação de usuário. Se o código de
-     * exceção for nulo, será recupera a mensagem de Throwable. A mensagem de
-     * Throwable pode ser nula também.
+     * Obtem a mensagem da excecao de autenticacao de usuario. Se o codigo de
+     * excecao for nulo, sera recupera a mensagem de Throwable. A mensagem de
+     * Throwable pode ser nula tambem.
      * @return
-     *      A mensagem de exceção.
+     *      A mensagem de excecao.
      */
     @Override
     public String getMessage(){
@@ -92,31 +92,31 @@ public class AutenticacaoUsuarioExcecao extends Exception{
               case 2:
                   return "Login : vazio";
               case 3:
-                  return "Login : caractere(s) inválido(s) - Não alfanuérico";
+                  return "Login : caractere(s) invalido(s) - Nao alfanuerico";
               case 4:
-                  return "Login : tamanho inferior ao mínimo(8)";
+                  return "Login : tamanho inferior ao minimo(8)";
               case 5:
-                  return "Login : tamanho superior ao máximo(12)";
+                  return "Login : tamanho superior ao maximo(12)";
               case 6:
                   return "Senha : nulo";
               case 7:
                   return "Senha : vazio";
               case 8:
-                  return "Senha : tamanho inferior ao mínimo(8)";
+                  return "Senha : tamanho inferior ao minimo(8)";
               case 9:
-                  return "Senha : tamanho superior ao máximo(12)";
+                  return "Senha : tamanho superior ao maximo(12)";
               case 10:
                   return "Login : Login incorreto";
               case 11:
                   return "Senha : Senha incorreta";
               case 12:
-                  return "Cadastro : Usuario já cadastrado";
+                  return "Cadastro : Usuario ja cadastrado";
               default:
                   return super.getMessage();
             }
         }
         return super.getMessage();
-    }// fim do método getMessage()
+    }// fim do metodo getMessage()
     
 }// fim da classe AutenticacaoUsuarioExcecao
 
