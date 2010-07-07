c85c4dad6b6fcd1842332c8a8f4e7ac5e551ad45
mvncius <mvncius@7978b9b3-e860-e8e4-5974-79d8b3795186>
2010-07-07 00:51:38 +0000

    a tela de login ta pronta 100%. testem!!!!

--- PostOfficeManager/src/graphic/Login.java
+++ PostOfficeManager/src/graphic/Login.java
@@ -1,5 +1,6 @@
 package graphic;
 
+import java.io.*;
 import servicosAutenticacaoGerencUsuario.AutenticacaoUsuario;
 
 /**
@@ -7,14 +8,14 @@ import servicosAutenticacaoGerencUsuario.AutenticacaoUsuario;
  */
 public class Login extends javax.swing.JFrame {
 	
-	private AutenticacaoUsuario autenticacao;
 	
-    public Login() throws Exception {
+	private AutenticacaoUsuario autenticacao;
+    public Login() throws IOException {
+    	autenticacao = new AutenticacaoUsuario();
         initComponents();
-        autenticacao = new AutenticacaoUsuario();
     }
 
-    @SuppressWarnings("unchecked")                         
+    @SuppressWarnings("unchecked")
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
@@ -23,9 +24,9 @@ public class Login extends javax.swing.JFrame {
         jLabel2 = new javax.swing.JLabel();
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
+        jLabel1 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setResizable(false);
 
         jPanel1.setBackground(new java.awt.Color(251, 252, 184));
 
@@ -36,7 +37,7 @@ public class Login extends javax.swing.JFrame {
         jPasswordField2.setText("senha");
         jPasswordField2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jPasswordField2MouseClicked(evt);
+                senhaTexto(evt);
             }
         });
 
@@ -47,7 +48,7 @@ public class Login extends javax.swing.JFrame {
         jTextField2.setText("Login");
         jTextField2.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
-                jTextField2MouseClicked(evt);
+                loginTexto(evt);
             }
         });
 
@@ -57,24 +58,34 @@ public class Login extends javax.swing.JFrame {
         jLabel2.setText("Login");
 
         jButton3.setBackground(new java.awt.Color(241, 231, 96));
-        jButton3.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        jButton3.setFont(new java.awt.Font("Verdana", 1, 14));
         jButton3.setForeground(new java.awt.Color(42, 43, 127));
-        jButton3.setText("Sair");
+        jButton3.setText("Cancelar");
         jButton3.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jButton3.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                quit(evt);
+                Cancelar(evt);
             }
         });
 
         jButton4.setBackground(new java.awt.Color(241, 231, 96));
-        jButton4.setFont(new java.awt.Font("Verdana", 1, 14)); // NOI18N
+        jButton4.setFont(new java.awt.Font("Verdana", 1, 14));
         jButton4.setForeground(new java.awt.Color(42, 43, 127));
         jButton4.setText("OK");
         jButton4.setHorizontalTextPosition(javax.swing.SwingConstants.CENTER);
         jButton4.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                logar(evt);
+                Confirmar(evt);
+            }
+        });
+
+        jLabel1.setForeground(new java.awt.Color(255, 0, 0));
+        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel1.setText("Usuário/senha errado");
+        jLabel1.setVisible(false);
+        jLabel1.addMouseListener(new java.awt.event.MouseAdapter() {
+            public void mouseClicked(java.awt.event.MouseEvent evt) {
+                desligarLabel(evt);
             }
         });
 
@@ -85,13 +96,14 @@ public class Login extends javax.swing.JFrame {
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
-                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addComponent(jPasswordField2, javax.swing.GroupLayout.PREFERRED_SIZE, 175, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, 112, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jPasswordField2, javax.swing.GroupLayout.PREFERRED_SIZE, 84, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(jLabel2)
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(jButton3)
-                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 299, Short.MAX_VALUE)
-                        .addComponent(jButton4)))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 262, Short.MAX_VALUE)
+                        .addComponent(jButton4))
+                    .addComponent(jLabel1))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -103,7 +115,9 @@ public class Login extends javax.swing.JFrame {
                 .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addComponent(jPasswordField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 93, Short.MAX_VALUE)
+                .addGap(30, 30, 30)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 46, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.CENTER)
                     .addComponent(jButton3)
                     .addComponent(jButton4))
@@ -122,49 +136,55 @@ public class Login extends javax.swing.JFrame {
         );
 
         pack();
-    }// </editor-fold>                        
+    }
 
-    private void jTextField2MouseClicked(java.awt.event.MouseEvent evt) {                                         
+    private void Confirmar(java.awt.event.ActionEvent evt) {                           
+    	try {
+			if (autenticacao.logaNoSistema(jTextField2.getText(), jPasswordField2.getText())) {
+			    setVisible(false);
+			    SelecionarGerenciamento selectGerenciamento = new SelecionarGerenciamento(jTextField2.getText());
+			    selectGerenciamento.setVisible(true);
+			    
+			} else {
+				jLabel1.setVisible(true);
+			}
+		} catch (Exception e) {
+		}
+    }                          
+
+    private void Cancelar(java.awt.event.ActionEvent evt) {                          
+        System.exit(0);
+    }                         
+
+    private void loginTexto(java.awt.event.MouseEvent evt) {
         if(jTextField2.isCursorSet() && jTextField2.getText().equals("Login")){
             jTextField2.setText("");
         }
         if(jTextField2.isCursorSet() && (jPasswordField2.getText().isEmpty())){
             jPasswordField2.setText("senha");
         }
-    }                                        
+        desligarLabel(evt);
+    }
 
-    private void jPasswordField2MouseClicked(java.awt.event.MouseEvent evt) {                                             
+    private void senhaTexto(java.awt.event.MouseEvent evt) {
         if(jPasswordField2.isCursorSet() && (jTextField2 == null || jTextField2.getText().isEmpty()) ){
             jTextField2.setText("Login");
         }
         if(jPasswordField2.isCursorSet() && jPasswordField2.getText().equals("senha")){
             jPasswordField2.setText("");
         }
-    }                                            
+        desligarLabel(evt);
+    }
 
-    private void quit(java.awt.event.ActionEvent evt) {                      
-        System.exit(0);
-    }                     
+    private void desligarLabel(java.awt.event.MouseEvent evt) {
+        jLabel1.setVisible(false);
+    }
 
-    private void logar(java.awt.event.ActionEvent evt) {                       
-    	try {
-    		String nomeDoUsuario = jTextField2.getText();
-    		autenticacao.logaNoSistema(nomeDoUsuario, jPasswordField2.getText());
-    		setVisible(false);
-    		SelecionarGerenciamento selectGerenciamento = new SelecionarGerenciamento(nomeDoUsuario);
-    		selectGerenciamento.setVisible(true);
-    	} catch (Exception e) {
-    		//Nessa parte, será registrada no log a tentativa de logar do usuário.
-		} 
-    }                      
-
-    // Variables declaration - do not modify                     
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPasswordField jPasswordField2;
     private javax.swing.JTextField jTextField2;
-    // End of variables declaration                   
-
 }
